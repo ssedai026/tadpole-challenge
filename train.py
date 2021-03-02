4423716569e9928dadc8e335a7802172db56a76c
Suman Sedai <ssedai@au1.ibm.com>
2021-03-03 10:52:05 +1100

    comments

--- train.py
+++ train.py
@@ -1,5 +1,7 @@
 from sklearn.ensemble import GradientBoostingRegressor, \
     GradientBoostingClassifier, RandomForestRegressor, RandomForestClassifier
+from sklearn.model_selection import GroupShuffleSplit
+
 import numpy as np
 
 from sklearn.metrics import accuracy_score
@@ -35,8 +37,8 @@ def getRFClassifier():
 
 
 if (__name__ == '__main__'):
-    from sklearn.model_selection import GroupShuffleSplit
 
+    #Load the training  data
     npzfile = np.load('data/cur_data_d1.npz')  #  data/d1.npz
     npzfile.files
     features = npzfile['features']
@@ -45,8 +47,8 @@ if (__name__ == '__main__'):
     features = np.nan_to_num(features)
     print ('#total number of patient', len(set(groups)))
 
+    #split into train val
     gss = GroupShuffleSplit(n_splits=1, train_size=.7, random_state=42) # change train size to 100% to train model for submission
-
 
     idx_trn, idx_test = next(gss.split(labels, groups=groups))
     print('#train patient', idx_trn.shape[0], ' #test patient', idx_test.shape[0])
@@ -55,7 +57,7 @@ if (__name__ == '__main__'):
     print ('X ', X.shape)
 
 
-    # ventricle volume prediction
+    # Training of ventricle volume prediction
     y_vv = labels[idx_trn, 0]
     reg, type = getGBMRegressor()  # RandomForestRegressor(n_estimators=500, max_depth=None, random_state=0, bootstrap=True)
     reg.fit(X, y_vv)
@@ -67,7 +69,7 @@ if (__name__ == '__main__'):
     print ('Mean absolute error for ventricle volume prediction using GBM regressor', np.mean(np.abs(yt_vv - ytvv_pred)))
 
 
-    # Disease state classification on validation set
+    #training of disease state classification on validation set
     y = labels[idx_trn, 1]
     yt = labels[idx_test, 1]
     clf, type = getGBMClassifier()  # RandomForestClassifier(n_estimators=100, max_depth=None, min_samples_split = 2, random_state = 0)
@@ -76,8 +78,10 @@ if (__name__ == '__main__'):
     print ('Accuracy for 3 class problem using GBM classifier', accuracy_score(yt, yt_pred, normalize=True))
     print ('AUC for 3 class problem using GBM classifier', multiclass_roc_auc_score(yt, yt_pred))
 
+    #save models
     if(not os.path.exists('models')):
         os.mkdir('models')
     pickle.dump(reg, open("models/d1_reg_vv.pickle", "wb"))
     pickle.dump(clf, open("models/d1_clf_dx.pickle", "wb"))
+    print ('Models saved to models directory')
 
